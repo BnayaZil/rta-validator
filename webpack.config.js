78bb5c81b8798404349a161441857a56ea24d017
Bnayazil <bnayazil@gmail.com>
2018-04-30 21:09:08 +0300

    Modify package name.

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,6 @@ module.exports = {
     entry: './index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'rtavalidator.js'
+        filename: 'rta-validator.js'
     },
 };
