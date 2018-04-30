bdc213ca390abddcf0716206106488114a559424
Bnayazil <bnayazil@gmail.com>
2018-05-01 00:33:45 +0300

    - Adding required dependencies. - Adding a build process using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: './index.js',
+    mode: 'production',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'rta-validator.js'
